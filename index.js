a49513ce06f47d5a3337b76bfc0bfde40ba8d3b0
Benjamin Krigel <benjamin.krigel@gmail.com>
2018-08-20 10:09:53 -0400

    tough to figure out the whole milliseconds thing

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name
+    this.startDate = new Date(startDate)
+  }
+  yearsExperienceFromBeginningOf(year) {
+    let endDate = new Date(year, 1, 1)
+    let experience = (endDate - this.startDate)/31536000000
+    return Math.ceil(experience)
+  }
+}
 
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+  avenueInteger(avenue){
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+    return eastWest.indexOf(avenue)
+  }
+  blocksTravelled(){
+    let horizontal = this.avenueInteger(this.endingLocation.horizontal) - this.avenueInteger(this.beginningLocation.horizontal)
+    let vertical = this.endingLocation.vertical - this.beginningLocation.vertical
+    return horizontal + vertical
+  }
+  estimatedTime(peak){
+    if (peak) {
+      return this.blocksTravelled() / 2
+    }
+    else {
+      return this.blocksTravelled() / 3
+    }
+  }
+}
